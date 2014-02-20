27e269776c84eb493337be3b7dc7e938bc2e7f80
Chris Forrette <chris@chrisforrette.com>
2014-02-19 22:44:10 -0800

    Connect, step, and transactions endpoint calls working

--- plaid/client.py
+++ plaid/client.py
@@ -1,4 +1,5 @@
 import json
+from urlparse import urljoin
 
 import requests
 
@@ -18,6 +19,10 @@ Maybe User/Transaction class?
 """
 
 class Client(object):
+    """
+    Python Plain API client https://plaid.io/
+    API version 2
+    """
 
     url = 'https://tartan.plaid.com'
 
@@ -28,6 +33,11 @@ class Client(object):
         ('citi', 'Citi',),
         ('wells', 'Wells Fargo',),
     )
+
+    endpoints = {
+        'connect': '/connect',
+        'step': '/connect/step'
+    }
 
     def __init__(self, client_id, secret, access_token=None):
         self.client_id = client_id
@@ -40,17 +50,93 @@ class Client(object):
 
     # Endpoints
 
-    def add_user(self, username, password, email, type, pretty=False, webhook=None, list=False):
+    def connect(self, account_type, username, password, email, \
+            pretty=False, webhook=None, mfa_list=False):
+        """
+        Add a bank account user/login to Plaid and receive an access token
+        unless a 2nd level of authentication is required, in which case 
+        an MFA (Multi Factor Authentication) question(s) is returned
+        """
+        url = urljoin(self.url, self.endpoints['connect'])
+
+        credentials = {
+            'username': username,
+            'password': password
+        }
+
+        options = {
+            'pretty': pretty,
+            'list': mfa_list
+        }
+
+        if webhook:
+            options['webhook'] = webhook
+
+        data = {
+            'client_id': self.client_id,
+            'secret': self.secret,
+            'type': account_type,
+            'credentials': json.dumps(credentials),
+            'email': email,
+            'options': json.dumps(options)
+        }
+
+        response = requests.post(url, data=data)
+
+        if response.ok:
+            json_data = json.loads(response.content)
+            if json_data.has_key('access_token'):
+                self.access_token = json_data['access_token']
+
+        return response
+
+    def step(self, account_type, mfa, pretty=False, send_method_type=None):
+        """
+        """
+        url = urljoin(self.url, '/connect/step')
+
+        options = {
+            'pretty': pretty
+        }
+
+        if send_method_type:
+            options['send_method'] = {'type': send_method_type}
+
+        data = {
+            'client_id': self.client_id,
+            'secret': self.secret,
+            'access_token': self.access_token,
+            'type': account_type,
+            'mfa': mfa,
+            'options': json.dumps(options)
+        }
+
+        return requests.post(url, data=data)
+
+    def delete_user(self):
         pass
 
-    def step(self, client_id, secret, access_token, type, mfa, send_method=False):
-        pass
+    def transactions(self, last=None, pretty=False):
+        """
+        Fetch a list of transactions
+        """
+        url = urljoin(self.url, '/connect')
 
-    def delete_user(self, client_id, secret, access_token):
-        pass
+        options = {
+            'pretty': pretty
+        }
 
-    def transactions(self, client_id, secret, access_token, last=None, pretty=False):
-        pass
+        if last:
+            options['last'] = last
+
+        data = {
+            'client_id': self.client_id,
+            'secret': self.secret,
+            'access_token': self.access_token,
+            'options': json.dumps(options)
+        }
+
+        return requests.get(url, data=data)
 
     def entity(self, entity_id, pretty=False):
         pass
