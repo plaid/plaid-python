6da95385aa1457cb4c4bfe4385036c72dd5377c1
David Chambers <dc@davidchambers.me>
2014-11-10 10:56:13 -0800

    Version 0.2.0

--- plaid/__init__.py
+++ plaid/__init__.py
@@ -1,1 +1,3 @@
+__version__ = '0.2.0'
+
 from client import Client, require_access_token
--- setup.py
+++ setup.py
@@ -3,17 +3,21 @@ from ez_setup import use_setuptools
 use_setuptools()
 
 from setuptools import setup, find_packages
+import plaid
 
+
+url = 'https://github.com/plaid/plaid-python'
 
 setup(
-    name='python-plaid',
-    version='0.1.0',
+    name='plaid-python',
+    version=plaid.__version__,
     description='Simple Python API client for Plaid',
     long_description='',
     keywords='api, client, plaid',
     author='Chris Forrette',
     author_email='chris@chrisforrette.com',
-    url='https://github.com/chrisforrette/python-plaid',
+    url=url,
+    download_url='{}/tarball/v{}'.format(url, plaid.__version__),
     license='MIT',
     packages=find_packages(exclude='tests'),
     package_data={'README': ['README.md']},
