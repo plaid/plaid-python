f0d12d26ac07652d6103850cb6b5a7d55fa5afad
Justin Myers <justmobilize@users.noreply.github.com>
2018-05-31 09:51:04 -0700

    Add support for Dwolla processor token

--- tests/integration/test_processor.py
+++ tests/integration/test_processor.py
@@ -12,10 +12,20 @@ from tests.integration.util import (
     create_client
 )
 
-def test_processor_token():
+
+def test_stripe_processor_token():
     client = create_client()
     # Just test the failure case - behavior here depends on the API keys used
     with pytest.raises(InvalidRequestError) as e:
         client.Processor.stripeBankAccountTokenCreate(
+            'fakeAccessToken', 'fakeAccountId')
+        assert e.code == 'INVALID_INPUT'
+
+
+def test_dwolla_processor_token():
+    client = create_client()
+    # Just test the failure case - behavior here depends on the API keys used
+    with pytest.raises(InvalidRequestError) as e:
+        client.Processor.dwollaBankAccountTokenCreate(
             'fakeAccessToken', 'fakeAccountId')
         assert e.code == 'INVALID_INPUT'
