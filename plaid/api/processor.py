2cbd3f509a0c53c5c3067df126651f7eed117f21
Justin Myers <justmobilize@users.noreply.github.com>
2018-07-05 10:15:08 -0700

    Add support for Dwolla processor token (#126)

--- plaid/api/processor.py
+++ plaid/api/processor.py
@@ -9,10 +9,24 @@ class Processor(API):
         Create a Stripe bank_account token for a given account ID
         (`HTTP docs <https://plaid.com/docs/link/stripe>`__)
 
-        :param  str     public_token:
+        :param  str     access_token:
         :param  str     account_id:
         '''
         return self.client.post('/processor/stripe/bank_account_token/create',
+                                {
+                                    'access_token': access_token,
+                                    'account_id': account_id,
+                                })
+
+    def dwollaBankAccountTokenCreate(self, access_token, account_id):
+        '''
+        Create a Dwolla processor token for a given account ID
+        (`HTTP docs <https://plaid.com/docs/link/dwolla>`__)
+
+        :param  str     access_token:
+        :param  str     account_id:
+        '''
+        return self.client.post('/processor/dwolla/processor_token/create',
                                 {
                                     'access_token': access_token,
                                     'account_id': account_id,
