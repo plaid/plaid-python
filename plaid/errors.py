795a7a2bacea1964e6082ed3151a16f1af379854
David Fish <dfish@plaid.com>
2019-02-11 14:14:01 -0800

    fix key error

--- plaid/errors.py
+++ plaid/errors.py
@@ -54,7 +54,7 @@ class PlaidError(Exception):
                    response['error_code'],
                    response['display_message'],
                    response['request_id'],
-                   response['causes'])
+                   response['causes'] if 'causes' in response else [])
 
 
 class PlaidCause(PlaidError):
