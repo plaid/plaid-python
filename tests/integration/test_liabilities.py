0d475d8739d36b3947119138da8a0bfc638015f8
Raymond Cano <rcano@plaid.com>
2019-07-17 11:45:48 -0700

    add test for account specification

--- tests/integration/test_liabilities.py
+++ tests/integration/test_liabilities.py
@@ -23,7 +23,15 @@ def teardown_module(module):
 
 def test_get():
     client = create_client()
+    # get liabilities for all accounts
     response = client.Liabilities.get(access_token)
     assert response['item'] is not None
     assert response['accounts'] is not None
     assert response['liabilities'] is not None
+
+    # get liabiliteis for specified account
+    account_id = response['accounts'][0]['account_id']
+    response = client.Liabilities.get(test_get,
+                                      account_ids=[account_id])
+    assert response['liabilities'] is not None
+    assert len(response['accounts']) == 1
