0da7cea44449bb10f792b57e9789336475a4f758
lukeguild <luke.guild@gmail.com>
2019-10-14 15:47:15 -0400

    add generic processor token creation method

--- plaid/api/processor.py
+++ plaid/api/processor.py
@@ -4,6 +4,26 @@ from plaid.api.api import API
 class Processor(API):
     '''Endpoints for creating processor tokens.'''
 
+    def tokenCreate(self, access_token, account_id, processor):
+        '''
+        Create a processor token for a given processor and account ID
+
+        :param  str     access_token:
+        :param  str     account_id:
+        :param  str     processor:
+        '''
+
+        endpoint = ('/processor/stripe/bank_account_token/create'
+                    if processor == 'stripe'
+                    else '/processor/{}/processor_token/create'.format(
+                        processor))
+
+        return self.client.post(endpoint,
+                                {
+                                    'access_token': access_token,
+                                    'account_id': account_id,
+                                })
+
     def stripeBankAccountTokenCreate(self, access_token, account_id):
         '''
         Create a Stripe bank_account token for a given account ID
