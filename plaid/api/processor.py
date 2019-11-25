3c4577c1bb8f3fb070fce8f7f8ce2a025d19855d
lukeguild <luke.guild@gmail.com>
2019-11-25 18:10:45 -0500

    Support stripe connected_account_id param

--- plaid/api/processor.py
+++ plaid/api/processor.py
@@ -4,18 +4,22 @@ from plaid.api.api import API
 class Processor(API):
     '''Endpoints for creating processor tokens.'''
 
-    def stripeBankAccountTokenCreate(self, access_token, account_id):
+    def stripeBankAccountTokenCreate(self, access_token, account_id,
+                                     connected_account_id):
         '''
         Create a Stripe bank_account token for a given account ID
         (`HTTP docs <https://plaid.com/docs/link/stripe>`__)
 
         :param  str     access_token:
         :param  str     account_id:
+        :param  str     connected_account_id:
         '''
         return self.client.post('/processor/stripe/bank_account_token/create',
                                 {
                                     'access_token': access_token,
                                     'account_id': account_id,
+                                    'connected_account_id':
+                                        connected_account_id,
                                 })
 
     def dwollaBankAccountTokenCreate(self, access_token, account_id):
