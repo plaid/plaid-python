4ed3ddd27f4c1845e0c1331e5a96d6713e11a380
Skylar Brown <sbrown@plaid.com>
2020-01-22 14:24:20 -0800

    more assertions

--- tests/integration/test_webhooks.py
+++ tests/integration/test_webhooks.py
@@ -11,3 +11,11 @@ def test_get_webhook_verification_key():
     client = create_client()
     pt_response = client.Webhooks.get_verification_key(WEBHOOK_VERIFICATION_KEY_ID)
     assert pt_response['key'] is not None
+    assert pt_response['key']['alg'] is not None
+    assert pt_response['key']['created_at'] is not None
+    assert pt_response['key']['crv'] is not None
+    assert pt_response['key']['kid'] is not None
+    assert pt_response['key']['kty'] is not None
+    assert pt_response['key']['use'] is not None
+    assert pt_response['key']['x'] is not None
+    assert pt_response['key']['y'] is not None
