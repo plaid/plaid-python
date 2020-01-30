f17ab010f2e163438923b7e3f61a1dd8e3b27603
Skylar Brown <sbrown@plaid.com>
2020-01-30 15:56:21 -0800

    fix indent

--- tests/integration/test_webhooks.py
+++ tests/integration/test_webhooks.py
@@ -10,8 +10,9 @@ from tests.integration.util import (
 
 def test_get_webhook_verification_key():
     client = create_client()
-    pt_response = client.Webhooks
-                        .get_verification_key(WEBHOOK_VERIFICATION_KEY_ID)
+    pt_response = client.Webhooks.get_verification_key(
+        WEBHOOK_VERIFICATION_KEY_ID
+    )
     assert pt_response['key'] is not None
     assert pt_response['key']['alg'] is not None
     assert pt_response['key']['created_at'] is not None
