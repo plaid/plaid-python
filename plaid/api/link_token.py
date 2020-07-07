26dc848b8e27b37564dc9c149749cd08454fbe6c
Andrew Chen <andrewchen@plaid.com>
2020-07-07 00:32:29 -0700

    try deconstructing

--- plaid/api/link_token.py
+++ plaid/api/link_token.py
@@ -9,22 +9,21 @@ class LinkToken(API):
 
         :param dict configs: A required dictionary to configure the Link token.
         '''
-        return self.client.post('/link/token/create', configs)
-        # return self.client.post('/link/token/create', {
-        #     'user': configs.get('user'),
-        #     'client_name': configs.get('client_name'),
-        #     'products': configs.get('products'),
-        #     'country_codes': configs.get('country_codes'),
-        #     'language': configs.get('language'),
-        #     'locale': configs.get('locale'),
-        #     'redirect_uri': configs.get('redirect_uri'),
-        #     'android_package_name': configs.get('android_package_name'),
-        #     'webhook': configs.get('webhook'),
-        #     'institution_id': configs.get('institution_id'),
-        #     'link_customization_name':
-        #      configs.get('link_customization_name'),
-        #     'access_token': configs.get('access_token'),
-        #     'account_filters': configs.get('account_filters'),
-        #     'cross_app_item_add': configs.get('cross_app_item_add'),
-        #     'payment_initiation': configs.get('payment_initiation'),
-        # })
+        #return self.client.post('/link/token/create', configs)
+        return self.client.post('/link/token/create', {
+            'user': configs.get('user'),
+            'client_name': configs.get('client_name'),
+            'products': configs.get('products'),
+            'country_codes': configs.get('country_codes'),
+            'language': configs.get('language'),
+            'locale': configs.get('locale'),
+            'redirect_uri': configs.get('redirect_uri'),
+            'android_package_name': configs.get('android_package_name'),
+            'webhook': configs.get('webhook'),
+            'institution_id': configs.get('institution_id'),
+            'link_customization_name': configs.get('link_customization_name'),
+            'access_token': configs.get('access_token'),
+            'account_filters': configs.get('account_filters'),
+            'cross_app_item_add': configs.get('cross_app_item_add'),
+            'payment_initiation': configs.get('payment_initiation'),
+        })
