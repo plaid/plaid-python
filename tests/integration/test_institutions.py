733e7d7454f29c9d407df6a57b020ae670abda67
Stephen Jayakar <sjayakar@plaid.com>
2020-10-28 13:14:44 -0700

    OpenAPI generated code at 10_28_20_13_14_40

--- tests/integration/test_institutions.py
+++ tests/integration/test_institutions.py
@@ -1,24 +1,21 @@
 from tests.integration.util import (
     create_client,
     SANDBOX_INSTITUTION,
-    SANDBOX_INSTITUTION_COUNTRY_CODE,
     SANDBOX_INSTITUTION_NAME,
 )
 
 
 def test_get():
     client = create_client()
-    response = client.Institutions.get(
-        [SANDBOX_INSTITUTION_COUNTRY_CODE], 3, offset=1)
+    response = client.Institutions.get(['US'], 3, offset=1)
     assert len(response['institutions']) == 3
 
 
 def test_get_with_include_optional_metadata():
     client = create_client()
-    response = client.Institutions.get(
-        [SANDBOX_INSTITUTION_COUNTRY_CODE], 3, offset=1, _options={
-            'include_optional_metadata': True,
-        })
+    response = client.Institutions.get(['US'], 3, offset=1, _options={
+        'include_optional_metadata': True,
+    })
     assert len(response['institutions']) == 3
     assert len(response['institutions'][0]['url']) > 0
     assert len(response['institutions'][0]['primary_color']) > 0
@@ -32,33 +29,28 @@ def test_get_with_include_optional_metadata():
 
 def test_get_by_id():
     client = create_client()
-    response = client.Institutions.get_by_id(
-        SANDBOX_INSTITUTION, [SANDBOX_INSTITUTION_COUNTRY_CODE])
+    response = client.Institutions.get_by_id(SANDBOX_INSTITUTION, ['US'])
     assert response['institution']['institution_id'] == SANDBOX_INSTITUTION
 
 
 def test_get_by_id_with_include_optional_metadata():
     client = create_client()
     response = client.Institutions.get_by_id(
-        SANDBOX_INSTITUTION, [SANDBOX_INSTITUTION_COUNTRY_CODE], _options={
+        SANDBOX_INSTITUTION,
+        ['US'],
+        _options={
             'include_optional_metadata': True,
         })
     assert response['institution']['institution_id'] == SANDBOX_INSTITUTION
-
-
-def test_search():
-    client = create_client()
-    response = client.Institutions.search(
-        SANDBOX_INSTITUTION_NAME, [SANDBOX_INSTITUTION_COUNTRY_CODE])
-    assert len(response['institutions']) >= 1
 
 
 def test_search_with_products():
     client = create_client()
     response = client.Institutions.search(
         SANDBOX_INSTITUTION_NAME,
-        [SANDBOX_INSTITUTION_COUNTRY_CODE],
-        products=['transactions'])
+        ['transactions'],
+        ['US'],
+    )
     assert len(response['institutions']) >= 1
 
 
@@ -66,7 +58,8 @@ def test_search_with_include_optional_metadata():
     client = create_client()
     response = client.Institutions.search(
         SANDBOX_INSTITUTION_NAME,
-        [SANDBOX_INSTITUTION_COUNTRY_CODE],
+        ['transactions'],
+        ['US'],
         _options={
             'include_optional_metadata': True,
         })
