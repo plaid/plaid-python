7885bfbbbbb09538f0b91075df4ba20165d6247d
Chase Farmer <cfarmer@plaid.com>
2020-12-03 14:27:25 -0800

    OpenAPI generated code at 12_03_20_14_27_23

--- plaid/generated_plaid/api/__init__.py
+++ plaid/generated_plaid/api/__init__.py
@@ -3,4 +3,5 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
+from plaid.generated_plaid.api.default_api import DefaultApi
 from plaid.generated_plaid.api.plaid_api import PlaidApi
